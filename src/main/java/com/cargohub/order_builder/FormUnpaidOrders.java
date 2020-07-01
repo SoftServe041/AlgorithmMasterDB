dbbadeec1c66343e45804f2f47f7ac30b9152cfc
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-07-01 22:43:28 +0300

    included cargo properties for forming unpaid orders

--- src/main/java/com/cargohub/order_builder/FormUnpaidOrders.java
+++ src/main/java/com/cargohub/order_builder/FormUnpaidOrders.java
@@ -2,7 +2,7 @@ package com.cargohub.order_builder;
 
 import com.cargoHub.neo4jGraph.model.RouteModel;
 import com.cargoHub.neo4jGraph.service.RouteService;
-import lombok.Getter;
+import com.cargohub.models.OrderModel;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,9 +13,11 @@ public class FormUnpaidOrders {
     @Autowired
     RouteService routeService;
     @Setter
-    Double pricePerKm;
+    Double pricePerKm = 1.4;
     @Setter
-    Double averageSpeed;
+    Double averageSpeed = 60d;
+    Double truckVolume = 80d;
+    Double carryingCapacity = 22d;
 
     public Map<String, List<UnpaidOrder>> formUnpaidOrders(String departure, String arrival) {
         Map<String,List<UnpaidOrder>> unpaidOrdersMap = new HashMap<>();
@@ -37,4 +39,40 @@ public class FormUnpaidOrders {
         unpaidOrdersMap.put("dateSorted", unpaidOrders);
         return unpaidOrdersMap;
     }
+
+    public Map<String, List<UnpaidOrder>> formUnpaidOrders(OrderModel orderModel) {
+        Map<String,List<UnpaidOrder>> unpaidOrdersMap = new HashMap<>();
+        List<RouteModel> routes = routeService.getRoute(orderModel.getDepartureHub(), orderModel.getArrivalHub());
+        List<UnpaidOrder> unpaidOrders = new ArrayList<>();
+        routes.forEach((routeModel) -> {
+            int price = (int) (routeModel.getDistance() * countPriceForRoute(orderModel));
+            double hours = routeModel.getDistance() / averageSpeed;
+            int days = (int) hours / 10 + (((int) hours % 10) < 5 ? 0 : 1);
+            LocalDate localDate = LocalDate.now().plusDays(days);
+            UnpaidOrder unpaidOrder = new UnpaidOrder();
+            unpaidOrder.setEstimatedDeliveryDate(localDate);
+            unpaidOrder.setPrice(price);
+            unpaidOrders.add(unpaidOrder);
+        });
+        unpaidOrders.sort(new PriceComparator());
+        unpaidOrdersMap.put("priceSorted", unpaidOrders);
+        unpaidOrders.sort(new DateComparator());
+        unpaidOrdersMap.put("dateSorted", unpaidOrders);
+        return unpaidOrdersMap;
+    }
+
+    private int countPriceForRoute(OrderModel orderModel) {
+        int volumeOfCargo = (int) Math.ceil(orderModel.getCargoHeight() * orderModel.getCargoLength() * orderModel.getCargoWidth());
+        double cub = carryingCapacity / truckVolume; // ? weight in 1 m^3 according to truck properties
+
+        double cargoWeight = orderModel.getCargoWeight() / 1000; // tonne
+
+        double admittedWeightForCargo = cub * volumeOfCargo; // determine how much is applicable for cargoVolume
+        while (admittedWeightForCargo < cargoWeight - 0.030) { // increase admittedWeight while it equals approximately to cargoWeight
+            admittedWeightForCargo = cub * (++volumeOfCargo);
+        }
+        int price = (int) Math.ceil(admittedWeightForCargo * pricePerKm);
+        return price;
+    }
+
 }
