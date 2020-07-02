364d49abeb8bdcca1e5ac4e74ed40eba66eaaa42
Fazliddin Makhsudov <fazl.makhsudov@gmail.com>
2020-07-02 14:15:59 +0300

    correlated algorithm in order to count in double

--- src/main/java/com/cargohub/order_builder/FormUnpaidOrders.java
+++ src/main/java/com/cargohub/order_builder/FormUnpaidOrders.java
@@ -64,14 +64,15 @@ public class FormUnpaidOrders {
     }
 
     private int countPriceForRoute(OrderModel orderModel) {
-        int volumeOfCargo = (int) Math.ceil(orderModel.getCargoHeight() * orderModel.getCargoLength() * orderModel.getCargoWidth());
+        double volumeOfCargo = orderModel.getCargoHeight() * orderModel.getCargoLength() * orderModel.getCargoWidth();
         double cub = carryingCapacity / truckVolume; // ? weight in 1 m^3 according to truck properties
 
         double cargoWeight = orderModel.getCargoWeight() / 1000; // tonne
 
         double admittedWeightForCargo = cub * volumeOfCargo; // determine how much is applicable for cargoVolume
         while (admittedWeightForCargo < cargoWeight - 0.030) { // increase admittedWeight while it equals approximately to cargoWeight
-            admittedWeightForCargo = cub * (++volumeOfCargo);
+            volumeOfCargo += 0.1;
+            admittedWeightForCargo = cub * volumeOfCargo;
         }
         int price = (int) Math.ceil(admittedWeightForCargo * pricePerKm);
         return price;
