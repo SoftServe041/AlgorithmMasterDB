5719f2d12a6508aa235d455272f47605d8079907
Alexey Tsarenko <42372423+AlexeyTsarenko@users.noreply.github.com>
2020-07-10 16:41:45 +0300

    simulation finished

--- src/main/java/com/cargohub/service/impl/OrderServiceImpl.java
+++ src/main/java/com/cargohub/service/impl/OrderServiceImpl.java
@@ -1,6 +1,5 @@
 package com.cargohub.service.impl;
 
-import com.cargohub.entities.Cargo;
 import com.cargohub.entities.Dimensions;
 import com.cargohub.entities.Hub;
 import com.cargohub.entities.CargoEntity;
@@ -19,6 +18,8 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.lang.reflect.Array;
+import java.text.DateFormat;
 import java.util.Random;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -87,26 +88,23 @@ public class OrderServiceImpl implements OrderService {
         }
         String arrName = orderEntity.getArrivalHub().getName();
         String depName = orderEntity.getDepartureHub().getName();
-        Hub arrivalHub;
-        Hub departureHub;
         Optional<Hub> optional = hubRepository.findByName(arrName);
         optional.ifPresent(orderEntity::setArrivalHub);
         optional = hubRepository.findByName(depName);
-        if(optional.isPresent()){
-            orderEntity.setDepartureHub(optional.get());
-        }
+        optional.ifPresent(orderEntity::setDepartureHub);
 
-        //orderEntity.setArrivalHub(hubRepository.findByName(orderEntity.getArrivalHub().getName()).get());
-        orderEntity.setTrackingId(generateTrackingId(orderEntity.getDepartureHub().getName(), orderEntity.getArrivalHub().getName(), orderEntity.getUserId()));
         for (CargoEntity cargo : orderEntity.getCargoEntities()
         ) {
             dimensionsRepository.save(cargo.getDimensions());
             cargoRepository.save(cargo);
         }
-        //hubRepository.save(orderEntity.getArrivalHub());
-        //hubRepository.save(orderEntity.getDepartureHub());
-        return repository.save(orderEntity);
+        OrderEntity returnEntity = repository.save(orderEntity);
+        List<CargoEntity> cargoList = orderEntity.getCargoEntities();
+        cargoList.forEach(x -> x.setOrderEntity(orderEntity));
+        cargoList.forEach(cargoRepository::save);
+        return returnEntity;
     }
+
 
     @Override
     public Page<OrderEntity> findAll(Pageable pageable) {
@@ -124,8 +122,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public void simulate() {
+        Random random = new Random();
         List<OrderEntity> list = new ArrayList<>();
-        for(int i = 0; i < 9; i++){
+        for (int i = 0; i < 9; i++) {
             list.add(new OrderEntity());
         }
         setHubs(list.get(0), "Kharkiv", "Poltava");
@@ -137,7 +136,6 @@ public class OrderServiceImpl implements OrderService {
         setHubs(list.get(6), "Kyiv", "Odesa");
         setHubs(list.get(7), "Odesa", "Kyiv");
         setHubs(list.get(8), "Odesa", "Sumy");
-        setHubs(list.get(9), "Sumy", "Pavlograd");
         list.forEach(x -> x.setUserId(1));
         list.forEach(x -> x.setPrice(1000.0));
         list.forEach(x -> {
@@ -147,60 +145,107 @@ public class OrderServiceImpl implements OrderService {
                 e.printStackTrace();
             }
         });
-        for(int i = 0; i < 3; i++){
+        int counter = 0;
+        for(OrderEntity order : list){
+            order.setTrackingId("ch" + random.nextInt(100) + counter++ + order.getArrivalHub().getName().hashCode()+ random.nextInt(1000));
+        }
+        for (int i = 0; i < 3; i++) {
             list.get(i).setDeliveryStatus(DeliveryStatus.PROCESSING);
+            try {
+                list.get(i).setCreated(new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").parse("2020.08.20 21:45:33"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
-        for(int i = 3; i < 6; i++){
+        for (int i = 3; i < 6; i++) {
             list.get(i).setDeliveryStatus(DeliveryStatus.ON_THE_WAY);
+            try {
+                list.get(i).setCreated(new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").parse("2020.07.10 21:45:33"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
-        for(int i = 6; i < list.size(); i++){
+        for (int i = 6; i < list.size(); i++) {
             list.get(i).setDeliveryStatus(DeliveryStatus.DELIVERED);
+            try {
+                list.get(i).setCreated(new SimpleDateFormat("yyyy.MM.dd HH:mm:ss").parse("2020.06.15 21:45:33"));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
         }
-
-
-
+        list.forEach(this::setCargo);
+        list.forEach(this::save);
     }
-    private static void setCargo(OrderEntity orderEntity, double weight, Integer width, Integer length, Integer height){
-        Cargo cargo = new Cargo();
-        cargo.setDeliveryStatus(orderEntity.getDeliveryStatus());
-        cargo.setStartingDestination(orderEntity.getDepartureHub().getName());
-        cargo.setFinalDestination(orderEntity.getArrivalHub().getName());
-        cargo.setWeight(weight);
+
+    private void setCargo(OrderEntity orderEntity) {
+        List<CargoEntity> cargoList = new ArrayList<>();
+        Random random = new Random();
+        int randCargosNumber = 5 + random.nextInt(6);
+        int randWeight = 100;
+        for (int i = 0; i < randCargosNumber; i++) {
+            CargoEntity cargo = new CargoEntity();
+            cargo.setDeliveryStatus(orderEntity.getDeliveryStatus());
+            cargo.setStartingDestination(orderEntity.getDepartureHub().getName());
+            cargo.setFinalDestination(orderEntity.getArrivalHub().getName());
+            cargo.setWeight((double) random.nextInt(randWeight+100));
+            cargo.setDimensions(getRandDimensions());
+            cargoList.add(cargo);
+        }
+        orderEntity.setCargoEntities(cargoList);
+    }
+
+    private Dimensions getRandDimensions() {
+        Random random = new Random();
+        int max = 10;
+        int rand = random.nextInt(max);
+        switch (rand) {
+            case 0: {
+              return returnSetDimensions(0.3, 0.6,0.6);
+            }
+            case 1: {
+                return returnSetDimensions(0.6, 0.6,0.6);
+            }
+            case 2: {
+                return returnSetDimensions(0.9, 0.9,0.9);
+            }
+            case 3: {
+                return returnSetDimensions(0.9, 1.2,1.5);
+            }
+            case 4: {
+                return returnSetDimensions(1.5, 1.5,1.8);
+            }
+            case 5: {
+                return returnSetDimensions(1.8, 1.8,1.8);
+            }
+            case 6: {
+                return returnSetDimensions(2.1, 1.8,2.4);
+            }
+            case 7: {
+                return returnSetDimensions(2.1, 2.1,2.4);
+            }
+            case 8: {
+                return returnSetDimensions(2.4, 2.1,2.4);
+            }
+            case 9: {
+                return returnSetDimensions(2.4, 2.4,2.4);
+            }
+        }
+         return null;
+    }
+    private Dimensions returnSetDimensions(double height, double width, double length){
         Dimensions dimensions = new Dimensions();
         dimensions.setHeight(height);
-        dimensions.setLength(length);
         dimensions.setWidth(width);
-        cargo.setDimensions(dimensions);
-        orderEntity.setCargo(cargo);
+        dimensions.setLength(length);
+        return dimensions;
     }
 
-    private static void setHubs(OrderEntity order, String dep, String arr){
+    private void setHubs(OrderEntity order, String dep, String arr) {
         Hub depHub = new Hub();
         Hub arrHub = new Hub();
         depHub.setName(dep);
         arrHub.setName(arr);
         order.setArrivalHub(arrHub);
         order.setDepartureHub(depHub);
-    }
-
-
-    private String generateTrackingId(String firstCity, String secondCity, long id) {
-        final Random random = new Random();
-
-        byte[] byteCity1 = firstCity.getBytes();
-        byte[] byteCity2 = secondCity.getBytes();
-
-        StringBuffer returnStr = new StringBuffer();
-        for (byte a : byteCity1) {
-            returnStr.append(a);
-        }
-        for (byte a : byteCity2) {
-            returnStr.append(a);
-        }
-        returnStr.append(id);
-        returnStr.append(random.nextInt());
-        returnStr.append(System.currentTimeMillis() % 100);
-
-        return returnStr.toString();
     }
 }
